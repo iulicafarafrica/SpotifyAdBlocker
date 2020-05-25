5657b79636a66c12634b836477baae41d252fe03
Hanz B <snowleopard756x@gmail.com>
2020-05-25 01:14:09 +0000

    Typo fix

--- NetUtilsSpotifyInjector/NetUtilsSpotifyInjector.cpp
+++ NetUtilsSpotifyInjector/NetUtilsSpotifyInjector.cpp
@@ -3,7 +3,7 @@
 // look for this dll, and access the entry point normally, unlike chrome_elf,
 // where exported functions are necessary to provide additional functions, 
 // because chrome_elf is a replacer (basically a hacked version), 
-// and netutils is an injectory 
+// and netutils is an injector
 
 #include "stdafx.h"
 
