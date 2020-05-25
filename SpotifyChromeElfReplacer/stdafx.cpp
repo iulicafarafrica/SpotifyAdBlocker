f9f993428808d14358b3e8753ca750fada33a2f1
Hanz B <snowleopard756x@gmail.com>
2020-05-25 23:33:05 +0000

    1/2 bad comments removed

--- SpotifyChromeElfReplacer/stdafx.cpp
+++ SpotifyChromeElfReplacer/stdafx.cpp
@@ -1,4 +1,3 @@
-// source file that includes commonly used Windows/library headers
 // SpotifyChromeElfReplacer.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
